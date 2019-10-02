bde19723f122c6faa3fad0f333ff6abab38b8989
Nikola Raicic <nikola.raicic@hotmail.com>
2019-10-02 19:10:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
 coupons.each do |coupon|
   if cart.keys.include? coupon[:item]
     if cart[coupon[:item]][:count] >= coupon[:num]
-      
+      new_name = "#"
 end
 
 def apply_clearance(cart)
