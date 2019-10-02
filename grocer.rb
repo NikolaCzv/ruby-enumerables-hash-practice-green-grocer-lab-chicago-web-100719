314db5e1000a6f34ff921b05c7cabba901580b4d
Nikola Raicic <nikola.raicic@hotmail.com>
2019-10-02 19:27:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,5 +47,11 @@ end
 
 def checkout(cart, coupons)
 consol_cart = consolidate_cart(cart)
-cart_with_coupons_applied = apply_coupons()
+cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+
+
+
+
+
+
 end
